ddc88cb4b80a4589a5fe1520f0f1fb7862857c56
Aleksandr Lundin <30766000+alexlundin@users.noreply.github.com>
2020-05-10 17:34:51 +0300

    Fix Ajax Loading

--- js/wares-script.js
+++ js/wares-script.js
@@ -2,21 +2,18 @@ const url = 'https://' + window.location.hostname;
 const mainLink = url + '/wp-json/wp/v2/wares/';
 const image = url + '/wp-json/wp/v2/media/';
 const elementsWares = document.querySelectorAll(".ajax-content");
-const idWares = [];
 const idList = [];
-
 elementsWares.forEach((item, i, elementsWares) => {
-    idWares.push(item.getAttribute('id'));
     idList.push(item.getAttribute('data-id'));
 });
 
-for (let i = 0; i < idWares.length; i++) {
+for (let i = 0; i < idList.length; i++) {
     let data = {
         action: "wares",
         id: idList[i],
     };
 
     jQuery.post(wares_ajax.wares_url, data, function (response) {
-        jQuery('#' + idWares[i]).html(response);
+        jQuery('div[data-id="'+ idList[i] +'"]').html(response);
     })
 }
