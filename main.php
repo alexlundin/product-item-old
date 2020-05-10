6e33ed4960a09c5bb0510d0d650614ffad2b1508
Aleksandr Lundin <30766000+alexlundin@users.noreply.github.com>
2020-05-10 21:44:34 +0300

    Fix Ajax Loading

--- main.php
+++ main.php
@@ -3,6 +3,7 @@
  * Plugin Name: Товары и Преимущества by Alex Lundin
  * Description: Плагин позволяет создавать товары и размещать и через шорткод как карточки с описанием и фото
  * Author: Alex Lundin
+ * Author URI: https://vk.com/aslundin
  * Version:     1.4.3
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
